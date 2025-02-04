21ce65c743f0f0967f77c16cff372377f5c648d9
ZeratoXD <ivan_cassio_dias@hotmail.com>
2025-02-04 10:32:27 -0300

    Config

---
+++ src/main/java/com/northwind/csv/NorthwindcsvApplication.java
@@ -0,0 +1,13 @@
+package com.northwind.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindcsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindcsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northwind/csv/NorthwindcsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northwind.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindcsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
