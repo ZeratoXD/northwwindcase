d97d6d87fcb21bf1a07d79492256c066bf1b7e55
ZeratoXD <ivan_cassio_dias@hotmail.com>
2025-02-04 14:48:56 -0300

    Model-Repository

---
+++ src/main/java/com/northwind/csv/model/Order.java
@@ -0,0 +1,33 @@
+package com.northwind.csv.model;
+
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customerId;
+    private Date orderDate;
+    private Date requiredDate;
+    private Date shippedDate;
+    private Double freight;
+    private String shipName;
+    private String shipAddress;
+    private String shipCity;
+    private String shipRegion;
+    private String shipPostalCode;
+    private String shipCountry;
+}
